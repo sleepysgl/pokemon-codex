7b3cd8bc242ee12262ac51aaca1a047617f48685
Seth Leones <seth.leoness@gmail.com>
2023-03-25 05:44:15 +0800

    Feat: created a function that loops thru each pokemon to get their stats and properties.

--- hw-react-pokedex/pokedex/src/components/Pokedex.js
+++ hw-react-pokedex/pokedex/src/components/Pokedex.js
@@ -8,7 +8,20 @@ function Pokedex() {
     const getAllPokemons = async () => {
         const response = await fetch(loadMore)
         const data = await response.json()
-        console.log (data)
+
+        setLoadmore(data.next)
+
+        function createPokemonObject (result) {
+            result.forEach( async (pokemon) => {
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+                const data = await response.json()
+
+                setAllPokemons(currentList => [...currentList, data])
+
+            });
+        }
+        createPokemonObject(data.results)
+        await console.log(allPokemons)
     }
 
     useEffect (() => {
@@ -16,7 +29,16 @@ function Pokedex() {
     }, []);
 
     return (
-        <h1>Pokedex</h1>
+        <div className = 'app-container'>
+            <h1>Pokedex</h1>
+            <div className = 'pokemon-container'>
+                <div className = 'all-container'>
+
+                </div>
+                <button className = 'load-more'>Load more</button>
+            </div>
+        </div>
+
     );
 }
 export default Pokedex;
